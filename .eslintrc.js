b0f7f3c775bbfe96a4f61d98c643733cd2167b16
wanghaonan12 <1470918223@qq.com>
2022-05-05 14:58:25 +0800

    fix: 修复运行中警告

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'space-before-function-paren': 'off',
-    'vue/multi-word-component-names':'off'
+    'vue/multi-word-component-names': 'off',
+    'eslint-disable-next-line': 'off',
+    'no-undef': 'off'
   }
 }
