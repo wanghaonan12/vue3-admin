11e5aa10bc35f710024350647c3b8604da6e4946
wanghaonan12 <1470918223@qq.com>
2022-05-05 15:25:45 +0800

    feat: 登陆后处理

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 const publicRoutes = [
   {
     path: '/',
-    component: () => import('@/views/login/index')
+    component: () => import('@/layout/index')
   }
 ]
 
