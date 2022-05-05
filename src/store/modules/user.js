8e9d90c9f5ad44e971dd90f324c61b006bf835b0
wanghaonan12 <1470918223@qq.com>
2022-05-05 19:15:45 +0800

    feat: 实现退出登录

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,7 +1,11 @@
 import { login, getUserInfo } from '@/api/sys'
 import md5 from 'md5'
-import { setItem, getItem } from '../../../utils/storage'
+import { setItem, getItem, removeAllItem } from '../../../utils/storage'
 import { TOKEN } from '@/constant'
+// 增加解构removeAllItem
+// 增加 导入路由
+import router from '@/router'
+
 export default {
   namespaced: true,
   state: () => ({
@@ -47,6 +51,12 @@ export default {
             reject(err)
           })
       })
+    },
+    logout() {
+      this.commit('user/setToken', '')
+      this.commit('user/setUserInfo', {})
+      removeAllItem()
+      router.push('/login')
     }
   }
 }
