cf69456d93abc7ffa234cb938320bf09b70beb6f
wanghaonan12 <1470918223@qq.com>
2022-05-06 22:40:30 +0800

    feat: 处理element plus主题变色

--- src/components/ThemeSelect/components/SelectColor.vue
+++ src/components/ThemeSelect/components/SelectColor.vue
@@ -21,7 +21,7 @@
 <script setup>
 import { defineEmits, ref } from 'vue'
 import { useStore } from 'vuex'
-
+import { generateNewStyle, writeNewStyle } from '../../../../utils/theme'
 const store = useStore()
 // 默认色值
 const mColor = ref(store.getters.mainColor)
@@ -66,6 +66,10 @@ const closed = () => {
  * 3. 关闭 dialog
  */
 const comfirm = async () => {
+  // 1.1 获取主题色
+  const newStyleText = await generateNewStyle(mColor.value)
+  // 1.2 写入最新主题色
+  writeNewStyle(newStyleText)
   // 2. 保存最新的主题色
   store.commit('theme/setMainColor', mColor.value)
   // 3. 关闭 dialog
