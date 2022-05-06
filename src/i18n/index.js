8bd8cd140bfeef4a404709941ada71dac50fb462
wanghaonan12 <1470918223@qq.com>
2022-05-06 22:12:32 +0800

    feat: 国际化缓存处理

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,6 +1,7 @@
 import { createI18n } from 'vue-i18n'
 import mZhLocale from './lang/zh'
 import mEnLocale from './lang/en'
+import store from '@/store'
 
 // 创建 message 数据源
 const messages = {
@@ -17,7 +18,6 @@ const messages = {
 }
 
 // 创建 locale 语言变量
-const locale = 'en'
 
 // 初始化 i18n 实例
 const i18n = createI18n({
@@ -25,9 +25,11 @@ const i18n = createI18n({
   legacy: false,
   // 全局注入 $t 函数
   globalInjection: true,
-  locale,
+  locale: getLanguage(),
   messages
 })
-
+function getLanguage() {
+  return store && store.getters && store.getters.language
+}
 // 导出 i18n 实例
 export default i18n
