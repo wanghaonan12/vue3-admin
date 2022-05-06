5b622af916d5c7c8d69b12f52d751b4e74b8a57b
wanghaonan12 <1470918223@qq.com>
2022-05-06 22:29:06 +0800

    feat: 创建SelectColor组件

--- src/components/ThemeSelect/index.vue
+++ src/components/ThemeSelect/index.vue
@@ -16,14 +16,22 @@
           {{ $t('msg.theme.themeColorChange') }}
         </el-dropdown-item>
       </el-dropdown-menu>
+
     </template>
   </el-dropdown>
   <!-- 展示弹出层 -->
-  <div></div>
+   <div>
+    <select-color v-model="selectColorVisible"></select-color>
+  </div>
 </template>
 
 <script setup>
-const handleSetTheme = command => {}
-</script>
+import SelectColor from './components/SelectColor.vue'
+import { ref } from 'vue'
 
+const selectColorVisible = ref(false)
+const handleSetTheme = command => {
+  selectColorVisible.value = true
+}
+</script>
 <style lang="scss" scoped></style>
