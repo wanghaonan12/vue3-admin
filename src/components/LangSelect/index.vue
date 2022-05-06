d33ee6a3ffbf85d7f9a2793946b8795ff1c29564
wanghaonan12 <1470918223@qq.com>
2022-05-06 21:56:13 +0800

    feat: 处理项目国际化内容

--- src/components/LangSelect/index.vue
+++ src/components/LangSelect/index.vue
@@ -5,7 +5,7 @@
     @command="handleSetLanguage"
   >
     <div>
-      <el-tooltip content="国际化" :effect="effect">
+      <el-tooltip :content="$t('msg.navBar.lang')" :effect="effect">
         <svg-icon icon="language" />
       </el-tooltip>
     </div>
@@ -32,7 +32,7 @@ defineProps({
   effect: {
     type: String,
     default: 'dark',
-    validator: function(value) {
+    validator: function (value) {
       // 这个值必须匹配下列字符串中的一个
       return ['dark', 'light'].indexOf(value) !== -1
     }
@@ -44,9 +44,9 @@ const language = computed(() => store.getters.language)
 
 // 切换语言的方法
 const i18n = useI18n()
-const handleSetLanguage = lang => {
+const handleSetLanguage = (lang) => {
   i18n.locale.value = lang
   store.commit('app/setLanguage', lang)
-  ElMessage.success('更新成功')
+  ElMessage.success(i18n.t('msg.toast.switchLangSuccess'))
 }
 </script>
