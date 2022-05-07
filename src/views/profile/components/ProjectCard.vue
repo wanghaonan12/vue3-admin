45525ebc70c3b67cd5631a212e0fa9fd7ad388f8
wanghaonan12 <1470918223@qq.com>
2022-05-07 09:26:20 +0800

    feat: 创建avatar头像组件

--- src/views/profile/components/ProjectCard.vue
+++ src/views/profile/components/ProjectCard.vue
@@ -1,41 +1,14 @@
 <template>
-  <div class="my-container">
-    <el-row>
-      <el-col :span="6">
-        <project-card class="user-card"></project-card>
-      </el-col>
-      <el-col :span="18">
-        <el-card>
-          <el-tabs v-model="activeName">
-            <el-tab-pane :label="$t('msg.profile.feature')" name="feature">
-              <feature />
-            </el-tab-pane>
-            <el-tab-pane :label="$t('msg.profile.chapter')" name="chapter">
-              <chapter />
-            </el-tab-pane>
-            <el-tab-pane :label="$t('msg.profile.author')" name="author">
-              <author />
-            </el-tab-pane>
-          </el-tabs>
-        </el-card>
-      </el-col>
-    </el-row>
+  <div class="">
+    <my-avatar :image="$store.getters.userInfo.avatar" :height="'100px'" :width="'100px'" :hoverable="false">
+      <div>Hello</div>
+      {{ $store.getters.userInfo.title }}
+    </my-avatar>
   </div>
 </template>
 
 <script setup>
-import ProjectCard from './components/ProjectCard.vue'
-import Chapter from './components/Chapter.vue'
-import Feature from './components/Feature.vue'
-import Author from './components/Author.vue'
-import { ref } from 'vue'
-const activeName = ref('feature')
+import MyAvatar from '@/components/MyAvatar'
 </script>
 
-<style lang="scss" scoped>
-.my-container {
-  .user-card {
-    margin-right: 20px;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
